c4735cee14bc2419a385b079efc328472034508a
Varun Pradeep <varunpradeep30@gmail.com>
2023-01-01 14:42:22 +0530

    in progress

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Shared from '../share/Share'
+import "./feed.css"
+
+export default function Feed() {
+  return (
+    <div className="feed">
+        <div className="feedWrapper">
+          <Shared/>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./topbar.css"
+import { Chat, Notifications, Person, Search } from "@material-ui/icons"
+
+export default function Topbar() {
+  return (
+    <div className='topbarContainer'>
+      <div className="topbarleft">
+        <span className="logo">VSocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className='searchbarIcon'/>
+          <input placeholder='Search...' className="searchInput" />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person/>
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat/>
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications/>
+            <span className="topbarIconBadge">3 </span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
+    </div>
+
+  )
+}
